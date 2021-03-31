c36b49e89f6ddbc91929d7ebb2a1834e41dfd484
Andrey Dor <andrewdor992@gmail.com>
2021-03-31 17:56:33 +0300

    auth module add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import {GraphQLModule} from "@nestjs/graphql";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
-    })
+    }),
+    AuthModule
   ],
 })
 export class AppModule {}
