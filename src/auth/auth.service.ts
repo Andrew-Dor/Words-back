490ded3327fcc5eda6d8fbff38ef817a9fdd80de
Andrey Dor <andrewdor992@gmail.com>
2021-04-05 16:58:32 +0300

    add signUp method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
 import { User } from './user.entity';
@@ -9,22 +9,31 @@ export class AuthService {
     constructor(
         @InjectRepository(User)
         private readonly userRepository: MongoRepository<User>,
-    ) {}
+    ) { }
+
+    // async getUserByEmail(email:string):Promise<User> {
+    //     return await this.userRepository.findOne({email});
+    // }
 
     //new user creation
-    async signUp(params: CreateUserParams):Promise<boolean> {
+    async signUp(params: CreateUserParams): Promise<boolean> {
+        const { email, name, password } = params;
+
+        const user = new User();
+        user.name = name;
+        user.password = password;
+        user.email = email;
+
         try {
-            const {email,name,password} = params;
-            const user = new User();
-            user.name = name;
-            user.password = password;
-            user.email = password;
-    
             await this.userRepository.save(user);
             return true;
-        } catch (err) {
-            throw new InternalServerErrorException();
+        } catch (error) {
+            if (error.code === 11000) {
+                throw new ConflictException('This email has already exists!');
+            } else {
+                throw new InternalServerErrorException();
+            }
         }
-
     }
+
 }
