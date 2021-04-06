efc4473dcf8a58936d76dfb040a5c94c04008eb5
Andrey Dor <andrewdor992@gmail.com>
2021-04-06 20:48:31 +0300

    add config files

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from "config";
 
 async function bootstrap() {
+    const serverConfig = config.get('server');    
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
-    await app.listen(3000);
+    await app.listen(process.env.PORT || serverConfig.port);
 }
 bootstrap();
